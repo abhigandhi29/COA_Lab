6f47f1bd6b624f193bc51d30f40a71d5c563c0e5
abhigandhi29 <abhigndhi29@gmail.com>
2021-09-04 23:12:52 +0530

    adding question2

--- Assign2/Q2Sol.s
+++ Assign2/Q2Sol.s
@@ -1,8 +1,9 @@
 .data
 
 # output strings
-input_msg:      .asciiz "Enter a no. : "
-output_msg:     .asciiz "GCD of the two no.s : "
+input_msg_1:    .asciiz "Enter the first positive integer: "
+input_msg_2:    .asciiz "Enter the second positive integer: "
+output_msg:     .asciiz "GCD of the two integers is: "
 error_msg:      .asciiz "Invalid Input"
 newline:        .asciiz "\n"
 
@@ -13,62 +14,65 @@ EXIT_CODE:           .word 10
 
 .text
 .globl main
-    main:
-        # Reading first no. n1
-        la $a0, input_msg
-        lw $v0, PRINT_STRING_CODE
-        syscall
-        lw $v0, READ_INT_CODE
-        syscall
-        move $t0, $v0
 
-        # Reading second no. n2
-        la $a0, input_msg
-        lw $v0, PRINT_STRING_CODE
-        syscall
-        lw $v0, READ_INT_CODE
-        syscall
-        move $t1, $v0
+main:
+    # Reading first no. n1
+    la $a0, input_msg_1
+    lw $v0, PRINT_STRING_CODE
+    syscall
+    lw $v0, READ_INT_CODE
+    syscall
+    move $s0, $v0
 
-        # invalid inputs
-        blt $t0, 1, invalid_
-        blt $t1, 1, invalid_
+    # Reading second no. n2
+    la $a0, input_msg_2
+    lw $v0, PRINT_STRING_CODE
+    syscall
+    lw $v0, READ_INT_CODE
+    syscall
+    move $s1, $v0
 
-        # while(n1 != n2)
-    loop:
-        beq $t1, $t0, end_loop
-        # if(n1 > n2)
-        bgt $t0, $t1, N1
-        # else
-        j N2
-        # n1 = n1-n2
-    N1: sub $t0, $t0, $t1
-        # loop continues
-        j loop
-        # n3 = n2-n1
-    N2: sub $t1, $t1, $t0
-        # loop continues
-        j loop
-        # loop ends
-    end_loop:
-        # Print results
-        la $a0, output_msg
-        lw $v0, PRINT_STRING_CODE
-        syscall
-        move $a0, $t0
-        lw $v0, PRINT_INT_CODE
-        syscall
-        j exit_
-    invalid_:
-        # Print error statement
-        la $a0, error_msg
-        lw $v0, PRINT_STRING_CODE
-        syscall
-        j exit_
-    exit_:
-        # Exit
-        la $a0, newline
-        lw $v0, PRINT_STRING_CODE
-        syscall
-        lw $v0, EXIT_CODE
-        syscall
+    # invalid inputs
+    blt $s0, 1, invalid_
+    blt $s1, 1, invalid_
+
+    bne $s0, $0, while_loop
+    move $s0, $s1
+    j end_loop
+    # while(n1 != n2)
+while_loop:
+    beq $s1, $0, end_loop 
+    bgt $s0, $s1, N1 # if(n1 > n2)
+
+    sub $s1, $s1, $s0 # n3 = n2-n1
+    j while_loop # loop continues
+    
+N1: 
+    sub $s0, $s0, $s1   # n1 = n1-n2
+    # loop continues
+    j while_loop
+    
+    
+end_loop: # loop ends
+    # Print results
+    la $a0, output_msg
+    lw $v0, PRINT_STRING_CODE
+    syscall
+    move $a0, $s0
+    lw $v0, PRINT_INT_CODE
+    syscall
+    j exit_
+
+invalid_:
+    # Print error statement
+    la $a0, error_msg
+    lw $v0, PRINT_STRING_CODE
+    syscall
+    j exit_
+exit_:
+    # Exit
+    la $a0, newline
+    lw $v0, PRINT_STRING_CODE
+    syscall
+    lw $v0, EXIT_CODE
+    syscall
