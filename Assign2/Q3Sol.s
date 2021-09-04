856dd5d5493f7e04cc6d4897037ca0bcba50d3d8
abhigandhi29 <abhigndhi29@gmail.com>
2021-09-04 23:02:16 +0530

    adding question3

--- Assign2/Q3Sol.s
+++ Assign2/Q3Sol.s
@@ -1,10 +1,10 @@
 .data
 
 # output strings
-input_msg:      .asciiz "Enter a no. : "
-output_msg_t:   .asciiz "Number is prime"
-output_msg_f:   .asciiz "Number is not prime"
-error_msg:      .asciiz "Invalid Input"
+input_msg:      .asciiz "Enter  a  positiveinteger greater than equals to 10: "
+output_msg_t:   .asciiz "Entered number is a prime number."
+output_msg_f:   .asciiz "Entered number is a Composite number."
+error_msg:      .asciiz "Invalid Input, Input must be greater than 10."
 newline:        .asciiz "\n"
 
 READ_INT_CODE:       .word 5
@@ -21,26 +21,26 @@ EXIT_CODE:           .word 10
         syscall
         lw $v0, READ_INT_CODE
         syscall
-        move $t0, $v0
+        move $s0, $v0
 
         # invalid inputs
-        blt $t0, 1, invalid_
+        blt $s0, 10, invalid_
 
         # loop control variable i
         li $t1, 1
 
     for_loop:
         # i = i+1
-        add $t1, $t1, 1
-        # if i >=n, exit
-        bge $t1, $t0, exit_t
+        add $t1, $t1, 1 # if i*i >n, exit
+        mul $t2, $t1, $t1 #
+        bgt $t2, $s0, exit_t
 
         # gcd b/w i and n
         li $t3, 0
-        add $t3, $t3, $t0
+        add $t3, $t3, $s0
         li $t4, 0
         add $t4, $t4, $t1
-        # li $t3, ($t0)
+        # li $t3, ($s0)
         # li $t4, ($t1)
 
         loop:
@@ -63,29 +63,25 @@ EXIT_CODE:           .word 10
         # check for another number
         j for_loop
 
-    exit_t :
-        # if is prime
+    exit_t : # if is prime
         la $a0, output_msg_t
         lw $v0, PRINT_STRING_CODE
         syscall
         j exit_
     
-    exit_f :
-        # if not prime
+    exit_f : # if not prime
         la $a0, output_msg_f
         lw $v0, PRINT_STRING_CODE
         syscall
         j exit_
 
-    invalid_:
-        # Print error statement
+    invalid_: # Print error statement
         la $a0, error_msg
         lw $v0, PRINT_STRING_CODE
         syscall
         j exit_
 
-    exit_:
-        # Exit
+    exit_: # Exit
         la $a0, newline
         lw $v0, PRINT_STRING_CODE
         syscall
